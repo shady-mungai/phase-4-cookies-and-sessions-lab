8ca72dca936d6a78d7fded0b688f38cd5fc9494e
shady-mungai <shadrackmungai10@gmail.com>
2023-03-21 18:21:19 +0300

    Assignment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :reset_page_views, only: [:show]
 
   def index
     articles = Article.all.includes(:user).order(created_at: :desc)
@@ -7,8 +8,18 @@ class ArticlesController < ApplicationController
   end
 
   def show
+
+    # cookies[:views] ||= 0
+    # cookies[:views] = cookies[:views].to_i + 1 
+    # cookies[:views].to_i + 1
+    session[:page_views] = session[:page_views].to_i + 1
     article = Article.find(params[:id])
+
+    if session[:page_views] <= 3
     render json: article
+    else 
+      render json: { error: "Unauthorized "}, status: :unauthorized
+    end
   end
 
   private
@@ -16,5 +27,11 @@ class ArticlesController < ApplicationController
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
   end
+
+  def reset_page_views
+    session[:page_views] = 0
+  end
+
+
 
 end
