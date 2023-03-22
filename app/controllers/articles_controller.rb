73650316424c0907504beeaa61a0a1f8aaec40ca
shady-mungai <shadrackmungai10@gmail.com>
2023-03-22 08:55:39 +0300

    Completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-  before_action :reset_page_views, only: [:show]
+  #before_action :reset_page_views, only: [:show]
 
   def index
     articles = Article.all.includes(:user).order(created_at: :desc)
@@ -12,7 +12,9 @@ class ArticlesController < ApplicationController
     # cookies[:views] ||= 0
     # cookies[:views] = cookies[:views].to_i + 1 
     # cookies[:views].to_i + 1
-    session[:page_views] = session[:page_views].to_i + 1
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+     
     article = Article.find(params[:id])
 
     if session[:page_views] <= 3
@@ -28,9 +30,9 @@ class ArticlesController < ApplicationController
     render json: { error: "Article not found" }, status: :not_found
   end
 
-  def reset_page_views
-    session[:page_views] = 0
-  end
+  # def reset_page_views
+  #   session[:page_views] = 0
+  # end
 
 
 
